636549a94b492dd41f319fc6d082672cfb7b67c0
yael <5780603i@gmail.com>
2024-06-27 16:13:49 +0300

    user

--- MortgageAPI/Program.cs
+++ MortgageAPI/Program.cs
@@ -1,3 +1,36 @@
+//using DataContext;
+//using Repositories.Interface;
+//using Service;
+
+//var builder = WebApplication.CreateBuilder(args);
+
+//// Add services to the container.
+
+//builder.Services.AddControllers();
+//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+//builder.Services.AddServices();
+//builder.Services.AddDbContext<IContext, Db>();
+
+//var app = builder.Build();
+
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+//app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+//app.Run();
+
+
 using DataContext;
 using Repositories.Interface;
 using Service;
@@ -14,9 +47,13 @@ builder.Services.AddServices();
 builder.Services.AddDbContext<IContext, Db>();
 
 
-
-
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowLocalhost4200",
+        builder => builder.WithOrigins("http://localhost:4200")
+                          .AllowAnyMethod()
+                          .AllowAnyHeader());
+});
 
 var app = builder.Build();
 
@@ -30,6 +67,9 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors("AllowLocalhost4200");
+
 
 app.MapControllers();
 
